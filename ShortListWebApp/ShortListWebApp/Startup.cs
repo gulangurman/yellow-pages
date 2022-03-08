e642b2c9a8622490d3fb087610758322bc4c33aa
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-08 16:28:24 +0300

    Added path base to use in a subdirectory

--- ShortListWebApp/ShortListWebApp/Startup.cs
+++ ShortListWebApp/ShortListWebApp/Startup.cs
@@ -33,6 +33,8 @@ namespace ShortListWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/shortlist");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
