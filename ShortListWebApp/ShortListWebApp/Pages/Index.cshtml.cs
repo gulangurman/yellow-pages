005b3b5b61d37e6f6fd10a6b129db3c307d74635
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-08 14:57:34 +0300

    Displayed today's posts in Home Page

--- ShortListWebApp/ShortListWebApp/Pages/Index.cshtml.cs
+++ ShortListWebApp/ShortListWebApp/Pages/Index.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ShortListWebApp.Data;
+using ShortListWebApp.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +14,18 @@ namespace ShortListWebApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly PostContext _postContext;
+        public IList<Post> Post;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, PostContext postContext)
         {
             _logger = logger;
+            _postContext = postContext;
         }
 
-        public void OnGet()
+        public async void OnGetAsync()
         {
-
+            Post = await _postContext.Post.ToListAsync();
         }
     }
 }
