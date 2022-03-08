7be5531002eae24003bc01aa1a62a1894ad8932f
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-08 13:54:16 +0300

    Added PostContext

---
+++ ShortListWebApp/ShortListWebApp/Data/PostContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShortListWebApp.Model;
+
+namespace ShortListWebApp.Data
+{
+    public class PostContext : DbContext
+    {
+        public PostContext(DbContextOptions<PostContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Post { get; set; }
+    }
+}
+  
