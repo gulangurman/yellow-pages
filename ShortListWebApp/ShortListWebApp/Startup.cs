95a01eea9179d21db346d9fc0bc4287817848023
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-08 13:57:40 +0300

    Fixed DbContext import

--- ShortListWebApp/ShortListWebApp/Startup.cs
+++ ShortListWebApp/ShortListWebApp/Startup.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ShortListWebApp.Data;
 
 namespace ShortListWebApp
 {
