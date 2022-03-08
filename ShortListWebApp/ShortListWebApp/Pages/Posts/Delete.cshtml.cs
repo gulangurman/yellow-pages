36132b6b2ac39d7616e334ef9bdbff8b6ac9a100
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-08 14:08:03 +0300

    Added Razor Pages for Post model

---
+++ ShortListWebApp/ShortListWebApp/Pages/Posts/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShortListWebApp.Data;
+using ShortListWebApp.Model;
+
+namespace ShortListWebApp.Pages.Posts
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ShortListWebApp.Data.PostContext _context;
+
+        public DeleteModel(ShortListWebApp.Data.PostContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Post Post { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Post = await _context.Post.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Post == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Post = await _context.Post.FindAsync(id);
+
+            if (Post != null)
+            {
+                _context.Post.Remove(Post);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
