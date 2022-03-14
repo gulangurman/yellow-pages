9483562716b0e983c9b7492e9f29216793899332
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-14 15:47:46 +0300

    Seed sample categories.

--- ShortListWebApp/ShortListMVC/Data/PostContext.cs
+++ ShortListWebApp/ShortListMVC/Data/PostContext.cs
@@ -15,6 +15,20 @@ namespace ShortListMVC.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+          new Category { Id = 1, Name = "Kayıp", Slug = "lost", Icon = "dinner" }
+        , new Category { Id = 2, Name = "Evcil Hayvanlar", Slug = "pets", Icon = "home" }
+        , new Category { Id = 3, Name = "Eleman Aranıyor", Slug = "hiring", Icon = "tshirt" }
+        , new Category { Id = 4, Name = "İkinci El", Slug = "secondhand", Icon = "travel" }
+        , new Category { Id = 5, Name = "Serbest Çalışanlar", Slug = "hiring", Icon = "jobs" }
+        , new Category { Id = 6, Name = "Etkinlikler", Slug = "hiring", Icon = "bullhorn" }
+        , new Category { Id = 7, Name = "Ev yapımı", Slug = "handmade", Icon = "bolt" }
+        );
+        }
+
         public DbSet<Post> Post { get; set; }
         public DbSet<Category> Category { get; set; }
     }
---
+++ ShortListWebApp/ShortListMVC/Migrations/Post/20220314124138_SeedCategories.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShortListMVC.Migrations.Post
+{
+    public partial class SeedCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 1, "dinner", "Kayıp", "lost" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 2, "home", "Evcil Hayvanlar", "pets" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 3, "tshirt", "Eleman Aranıyor", "hiring" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 4, "travel", "İkinci El", "secondhand" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 5, "jobs", "Serbest Çalışanlar", "hiring" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 6, "bullhorn", "Etkinlikler", "hiring" });
+
+            migrationBuilder.InsertData(
+                table: "Category",
+                columns: new[] { "Id", "Icon", "Name", "Slug" },
+                values: new object[] { 7, "bolt", "Ev yapımı", "handmade" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Category",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
