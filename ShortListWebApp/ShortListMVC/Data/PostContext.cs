ba33612f85c8a8a1495d6aeae54ef8cf7ef79f41
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-16 16:56:08 +0300

    Added sample posts seed.

--- ShortListWebApp/ShortListMVC/Data/PostContext.cs
+++ ShortListWebApp/ShortListMVC/Data/PostContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ShortListMVC.Models;
 
@@ -18,6 +19,8 @@ namespace ShortListMVC.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            string TEST_USER_ID = "02174cf0–9412–4cfe - afbf - 59f706d72cf6";
+
             modelBuilder.Entity<Category>().HasData(
           new Category { Id = 1, Name = "Kayıp", Slug = "lost", Icon = "dinner" }
         , new Category { Id = 2, Name = "Evcil Hayvanlar", Slug = "pets", Icon = "home" }
@@ -27,6 +30,50 @@ namespace ShortListMVC.Data
         , new Category { Id = 6, Name = "Etkinlikler", Slug = "hiring", Icon = "bullhorn" }
         , new Category { Id = 7, Name = "Ev yapımı", Slug = "handmade", Icon = "bolt" }
         );
+            
+
+            modelBuilder.Entity<Post>().HasData(
+                new Post
+                {
+                    Id = 1,
+                    CategoryId = 1,
+                    Title = "Kayıp kedi",
+                    Tags = "kedi",
+                    Content = "Kedimiz Bıdık kaybolmuştur.",
+                    ImageUrl = "https://www.markuptag.com/images/image-six.jpg",
+                    AccountId = TEST_USER_ID
+                },
+                new Post
+                {
+                    Id = 2,
+                    CategoryId = 3,
+                    Title = "Eleman aranıyor",
+                    Tags = "eleman",
+                    Content = "Acil eleman aranıyor.",
+                    ImageUrl = "https://www.markuptag.com/images/image-two.jpg",
+                    AccountId = TEST_USER_ID
+                },
+                 new Post
+                 {
+                     Id = 3,
+                     CategoryId = 4,
+                     Title = "Satılık telefon",
+                     Tags = "telefon",
+                     Content = "İkinci el cep telefonu.",
+                     ImageUrl = "https://www.markuptag.com/images/image-three.jpg",
+                     AccountId = TEST_USER_ID
+                 },
+                  new Post
+                  {
+                      Id = 4,
+                      CategoryId = 6,
+                      Title = "Sahil konserleri",
+                      Tags = "konser",
+                      Content = "Sahilde konser etkinliğinde buluşuyoruz.",
+                      ImageUrl = "https://www.markuptag.com/images/image-four.jpg",
+                      AccountId = TEST_USER_ID
+                  }
+                );
         }
 
         public DbSet<Post> Post { get; set; }
---
+++ ShortListWebApp/ShortListMVC/Migrations/Post/20220316135341_AddedSamplePostsSeed.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShortListMVC.Migrations.Post
+{
+    public partial class AddedSamplePostsSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Post",
+                columns: new[] { "Id", "AccountId", "CategoryId", "Content", "ImageUrl", "Tags", "Title" },
+                values: new object[] { 1, "02174cf0–9412–4cfe - afbf - 59f706d72cf6", 1, "Kedimiz Bıdık kaybolmuştur.", "https://www.markuptag.com/images/image-six.jpg", "kedi", "Kayıp kedi" });
+
+            migrationBuilder.InsertData(
+                table: "Post",
+                columns: new[] { "Id", "AccountId", "CategoryId", "Content", "ImageUrl", "Tags", "Title" },
+                values: new object[] { 2, "02174cf0–9412–4cfe - afbf - 59f706d72cf6", 3, "Acil eleman aranıyor.", "https://www.markuptag.com/images/image-two.jpg", "eleman", "Eleman aranıyor" });
+
+            migrationBuilder.InsertData(
+                table: "Post",
+                columns: new[] { "Id", "AccountId", "CategoryId", "Content", "ImageUrl", "Tags", "Title" },
+                values: new object[] { 3, "02174cf0–9412–4cfe - afbf - 59f706d72cf6", 4, "İkinci el cep telefonu.", "https://www.markuptag.com/images/image-three.jpg", "telefon", "Satılık telefon" });
+
+            migrationBuilder.InsertData(
+                table: "Post",
+                columns: new[] { "Id", "AccountId", "CategoryId", "Content", "ImageUrl", "Tags", "Title" },
+                values: new object[] { 4, "02174cf0–9412–4cfe - afbf - 59f706d72cf6", 6, "Sahilde konser etkinliğinde buluşuyoruz.", "https://www.markuptag.com/images/image-four.jpg", "konser", "Sahil konserleri" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
