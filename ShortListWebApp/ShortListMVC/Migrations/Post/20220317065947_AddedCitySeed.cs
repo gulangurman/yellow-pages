41652776d9ebe0c18742eec8ad78215fb2b3ba65
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-17 10:04:10 +0300

    City seed data migration.

---
+++ ShortListWebApp/ShortListMVC/Migrations/Post/20220317065947_AddedCitySeed.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShortListMVC.Migrations.Post
+{
+    public partial class AddedCitySeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "City",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "06", "Ankara" });
+
+            migrationBuilder.InsertData(
+                table: "City",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "34", "İstanbul" });
+
+            migrationBuilder.InsertData(
+                table: "City",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { "35", "İzmir" });
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CityId",
+                value: "06");
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CityId",
+                value: "34");
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CityId",
+                value: "35");
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CityId",
+                value: "34");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: "06");
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: "34");
+
+            migrationBuilder.DeleteData(
+                table: "City",
+                keyColumn: "Id",
+                keyValue: "35");
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CityId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CityId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CityId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Post",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CityId",
+                value: null);
+        }
+    }
+}
