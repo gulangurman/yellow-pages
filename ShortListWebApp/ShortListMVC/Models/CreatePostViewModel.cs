391d3b306785b5f3771f253082eb2327f2259c8f
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-17 16:46:02 +0300

    Fixed nullable fields.

--- ShortListWebApp/ShortListMVC/Models/CreatePostViewModel.cs
+++ ShortListWebApp/ShortListMVC/Models/CreatePostViewModel.cs
@@ -4,8 +4,8 @@
     {
         public string Title { get; set; }
         public string Content { get; set; }
-        public string ImageUrl { get; set; }
-        public string Tags { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Tags { get; set; }
         public int? CategoryId { get; set; }
         public string? CityId { get; set; }
     }
