51ad69317c1a30ae83891ee10b0b78c6f4a57284
Gulan Gurman <76517407+gulangurman@users.noreply.github.com>
2022-03-17 10:03:12 +0300

    Added City seed data.

--- ShortListWebApp/ShortListMVC/Data/PostContext.cs
+++ ShortListWebApp/ShortListMVC/Data/PostContext.cs
@@ -30,50 +30,59 @@ namespace ShortListMVC.Data
         , new Category { Id = 6, Name = "Etkinlikler", Slug = "hiring", Icon = "bullhorn" }
         , new Category { Id = 7, Name = "Ev yapımı", Slug = "handmade", Icon = "bolt" }
         );
-            
+            modelBuilder.Entity<City>().HasData(
+                new City { Id = "06", Name = "Ankara" }
+              , new City { Id = "34", Name = "İstanbul" }
+              , new City { Id = "35", Name = "İzmir" }
+                );
+
 
             modelBuilder.Entity<Post>().HasData(
-                new Post
-                {
-                    Id = 1,
-                    CategoryId = 1,
-                    Title = "Kayıp kedi",
-                    Tags = "kedi",
-                    Content = "Kedimiz Bıdık kaybolmuştur.",
-                    ImageUrl = "https://www.markuptag.com/images/image-six.jpg",
-                    AccountId = TEST_USER_ID
-                },
-                new Post
-                {
-                    Id = 2,
-                    CategoryId = 3,
-                    Title = "Eleman aranıyor",
-                    Tags = "eleman",
-                    Content = "Acil eleman aranıyor.",
-                    ImageUrl = "https://www.markuptag.com/images/image-two.jpg",
-                    AccountId = TEST_USER_ID
-                },
                  new Post
                  {
-                     Id = 3,
-                     CategoryId = 4,
-                     Title = "Satılık telefon",
-                     Tags = "telefon",
-                     Content = "İkinci el cep telefonu.",
-                     ImageUrl = "https://www.markuptag.com/images/image-three.jpg",
+                     Id = 1,
+                     CategoryId = 1,
+                     Title = "Kayıp kedi",
+                     Tags = "kedi",
+                     Content = "Kedimiz Bıdık kaybolmuştur.",
+                     ImageUrl = "https://www.markuptag.com/images/image-six.jpg",
+                     CityId = "06",
+                     AccountId = TEST_USER_ID
+                 },
+                 new Post
+                 {
+                     Id = 2,
+                     CategoryId = 3,
+                     Title = "Eleman aranıyor",
+                     Tags = "eleman",
+                     Content = "Acil eleman aranıyor.",
+                     ImageUrl = "https://www.markuptag.com/images/image-two.jpg",
+                     CityId = "34",
                      AccountId = TEST_USER_ID
                  },
                   new Post
                   {
-                      Id = 4,
-                      CategoryId = 6,
-                      Title = "Sahil konserleri",
-                      Tags = "konser",
-                      Content = "Sahilde konser etkinliğinde buluşuyoruz.",
-                      ImageUrl = "https://www.markuptag.com/images/image-four.jpg",
+                      Id = 3,
+                      CategoryId = 4,
+                      Title = "Satılık telefon",
+                      Tags = "telefon",
+                      Content = "İkinci el cep telefonu.",
+                      ImageUrl = "https://www.markuptag.com/images/image-three.jpg",
+                      CityId = "35",
                       AccountId = TEST_USER_ID
-                  }
-                );
+                  },
+                   new Post
+                   {
+                       Id = 4,
+                       CategoryId = 6,
+                       Title = "Sahil konserleri",
+                       Tags = "konser",
+                       Content = "Sahilde konser etkinliğinde buluşuyoruz.",
+                       ImageUrl = "https://www.markuptag.com/images/image-four.jpg",
+                       CityId = "34",
+                       AccountId = TEST_USER_ID
+                   }
+                 );
         }
 
         public DbSet<Post> Post { get; set; }
